985da5e3f4f9abba04683633927072da75e0bf06
Garrett Martin <martingv19@gcc.edu>
2023-03-17 18:07:17 -0400

    Setup 2d array of numbers based on generated 2d array of bools, doesnt account for mines yet

--- GenerateMap.py
+++ GenerateMap.py
@@ -1,21 +1,109 @@
 import random as r
 
-def generate(width: int, height: int, numBomb: int) -> list[list[bool]]:
-    arr = [[False for row in range(width)] for column in range(height)]
+def generateBool(width: int, height: int, numBomb: int) -> list[list[bool]]:
+    arr = [[False for row in range(height)] for column in range(width)]
     placedBomb: int = 0
     
     position: int = 0
 
     while (placedBomb < numBomb) :
         position += r.randint(0, 7)
-        if (arr[position % width][position % height]):
+        if (arr[position // height][position % width]):
             continue
-        arr[position % width][position % height] = True
+        arr[position // height][position % width] = True
         placedBomb += 1
+    return arr
+
+
+def generateBoard(boolMap: list[list[bool]]) -> list[list[int]]:
+    arr = [[0 for row in range(len(boolMap[0]))] for column in range(len(boolMap))]
+
+    position: int = 0
+
+    for row in boolMap:
+        for val in row:
+            if val:
+                incrementNeighbors(position, arr)
+            position += 1
 
 
 
+    return arr
 
-    print(arr)
+def incrementNeighbors(position: int, array: list[list[int]]):
+    #Only enter here if position has a min
+    height: int = len(array)
+    width: int = len(array[0])
+    x = position // height
+    y = position % width
 
-generate(5,5,3)
+    if (x == 0):
+        #Top
+        if (y == 0):
+            #Top left
+            array[x + 1][y] += 1
+            array[x + 1][y - 1] += 1
+            array[x][y-1] += 1
+        elif (y == width - 1):
+            #Top right
+            array[x + 1][y] += 1
+            array[x + 1][y - 1] += 1
+            array[x][y - 1] += 1
+        else:
+            #Top side
+            array[x + 1][y] += 1
+            array[x + 1][y + 1] += 1
+            array[x][y + 1] += 1
+            array[x + 1][y - 1] += 1
+            array[x][y-1] += 1
+    elif (x == height - 1):
+        if (y == 0):
+            #Bottom left
+            array[x - 1][y] += 1
+            array[x - 1][y - 1] += 1
+            array[x][y-1] += 1
+        elif (y == width - 1):
+            #Bottom right
+            array[x - 1][y] += 1
+            array[x - 1][y - 1] += 1
+            array[x][y - 1] += 1
+        else:
+            #Bottom side
+            array[x - 1][y] += 1
+            array[x - 1][y + 1] += 1
+            array[x][y + 1] += 1
+            array[x - 1][y - 1] += 1
+            array[x][y-1] += 1
+    elif y == 0:
+        #Left side
+        array[x + 1][y] += 1
+        array[x + 1][y + 1] += 1
+        array[x][y + 1] += 1
+        array[x - 1][y] += 1
+        array[x - 1][y + 1] += 1
+    elif y == width - 1:
+        #Right Side
+        array[x + 1][y] += 1
+        array[x + 1][y - 1] += 1
+        array[x][y - 1] += 1
+        array[x - 1][y] += 1
+        array[x - 1][y - 1] += 1
+    else:
+        array[x - 1][y] += 1
+        array[x - 1][y + 1] += 1
+        array[x][y + 1] += 1
+        array[x - 1][y - 1] += 1
+        array[x][y-1] += 1
+        array[x + 1][y] += 1
+        array[x + 1][y + 1] += 1
+        array[x + 1][y - 1] += 1
+            
+
+
+boolMap = generateBool(5, 5, 3)
+
+for row in boolMap:
+    print(row)
+
+for row in generateBoard(boolMap):
+    print(row)
