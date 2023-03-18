deb65160378a0ee42ed71dad0084cef4abf74759
Garrett Martin <martingv19@gcc.edu>
2023-03-18 14:51:36 -0400

    fixes

--- MakeBoard.py
+++ MakeBoard.py
@@ -2,14 +2,14 @@ from numpy import zeros
 from random import randint
 import Main
 
-def makeBoard(self, rows, cols, mines):
+def makeBoard(rows, cols, mines):
     board = zeros((rows, cols), int)
 
     for _ in range(0, mines):
         # find a space that isn't already a mine
         m = (randint(0, rows-1), randint(0, cols-1))
 
-        while board[m[0]][m[1]] == -1 or (m[0],m[1]):
+        while board[m[0]][m[1]] == -1:
             m = (randint(0, rows-1), randint(0, cols-1))
 
         r = m[0]
@@ -21,6 +21,6 @@ def makeBoard(self, rows, cols, mines):
 
         neighbors = Main.findNeighbors(r,c, rows, cols)
         for row, col in neighbors:
-            board[row][col] if board[row][col] != -1 else 0
+            board[row][col] += 1 if board[row][col] != -1 else 0
 
     return board
