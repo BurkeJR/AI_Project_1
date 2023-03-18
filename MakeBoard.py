4d8812ba7ef3a41f4206ce4789a5ee734d2b8009
Garrett Martin <martingv19@gcc.edu>
2023-03-18 14:48:03 -0400

    Used findNeighbors to improve makeBoard

--- MakeBoard.py
+++ MakeBoard.py
@@ -1,35 +1,26 @@
 from numpy import zeros
 from random import randint
+import Main
 
-def makeBoard(rows, cols, mines):
+def makeBoard(self, rows, cols, mines):
     board = zeros((rows, cols), int)
 
     for _ in range(0, mines):
         # find a space that isn't already a mine
         m = (randint(0, rows-1), randint(0, cols-1))
-        while board[m[0]][m[1]] == -1:
+
+        while board[m[0]][m[1]] == -1 or (m[0],m[1]):
             m = (randint(0, rows-1), randint(0, cols-1))
 
         r = m[0]
         c = m[1]
 
+        
         # add the mine and increment its neighbors
         board[r][c] = -1
-        if r > 0:
-            board[r-1][c] += 1 if board[r-1][c] != -1 else 0
-            if c > 0:
-                board[r-1][c-1] += 1 if board[r-1][c-1] != -1 else 0
-            if c < cols - 1:
-                board[r-1][c+1] += 1 if board[r-1][c+1] != -1 else 0
-        if c > 0:
-            board[r][c-1] += 1 if board[r][c-1] != -1 else 0
-        if c < cols - 1:
-            board[r][c+1] += 1 if board[r][c+1] != -1 else 0
-        if r < rows - 1:
-            board[r+1][c] += 1 if board[r+1][c] != -1 else 0
-            if c > 0:
-                board[r+1][c-1] += 1 if board[r+1][c-1] != -1 else 0
-            if c < cols - 1:
-                board[r+1][c+1] += 1 if board[r+1][c+1] != -1 else 0
+
+        neighbors = Main.findNeighbors(r,c, rows, cols)
+        for row, col in neighbors:
+            board[row][col] if board[row][col] != -1 else 0
 
     return board
