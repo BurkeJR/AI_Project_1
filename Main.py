a8f8677e1668a8810f94f91a774aa5a3a66fd90b
Garrett Martin <martingv19@gcc.edu>
2023-03-19 16:34:16 -0400

    Cleanup Main.py

--- Main.py
+++ Main.py
@@ -11,16 +11,11 @@ def main():
 
     board = Board(rows, cols, mines, startr, startc)
     game_board = board.board
-    revealed_board = np.asarray([[(game_board[j][i].number if game_board[j][i].number != -1 else '*') for i in range(cols)] for j in range(rows)])
-
     board.uncover(startr, startc)
-
-    print(revealed_board)
+    
     print(board)
 
-    while True:
-        if sum((1 for row in game_board for x in row if not x.uncovered)) == mines:
-            break
+    while not board.has_won():
 
         val = input("g for guess, f for flag: ")
 
